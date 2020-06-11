b3d650e61720e838e5d13fa921d3167c716d24d7
Valchee <matkowski2@gmail.com>
2020-06-11 11:31:38 +0200

    Make use of reusable components

--- src/index.js
+++ src/index.js
@@ -2,28 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail'
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author={faker.name.findName()}
-        date={faker.date.past(1)}
-        avatar={faker.image.avatar()}
-        content={faker.lorem.sentence()}
-      />
-      <CommentDetail
-        author={faker.name.findName()}
-        date={faker.date.past(1)}
-        avatar={faker.image.avatar()}
-        content={faker.lorem.sentence()}
-      />
-      <CommentDetail
-        author={faker.name.findName()}
-        date={faker.date.past(1)}
-        avatar={faker.image.avatar()}
-        content={faker.lorem.sentence()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.findName()}
+          date={faker.date.past(1)}
+          avatar={faker.image.avatar()}
+          content={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.findName()}
+          date={faker.date.past(1)}
+          avatar={faker.image.avatar()}
+          content={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.findName()}
+          date={faker.date.past(1)}
+          avatar={faker.image.avatar()}
+          content={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
     </div>
   );
 }
