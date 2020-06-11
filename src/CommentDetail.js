c29f641a1b0437e20fe43ff9ba003a5af70b978e
Valchee <matkowski2@gmail.com>
2020-06-11 11:11:54 +0200

    Pass comment details in props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import faker from 'faker';
 
-const CommentDetail = () => {
+const CommentDetail = props => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
-          Sam
+          {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00PM</span>
+          <span className="date">{format_date(props.date)}</span>
         </div>
-        <div className="text">Nice blog post!</div>
+        <div className="text">{props.content}</div>
       </div>
     </div>
   );
+}
+
+function format_date(date) {
+  const dateTimeFormat = new Intl.DateTimeFormat('en', { year: 'numeric', month: '2-digit', day: '2-digit' });
+  const [{ value: month },,{ value: day },,{ value: year }] = dateTimeFormat .formatToParts(date );
+
+  return `${day}-${month}-${year}`;
 }
 
 export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail'
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        author={faker.name.findName()}
+        date={faker.date.past(1)}
+        avatar={faker.image.avatar()}
+        content={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        author={faker.name.findName()}
+        date={faker.date.past(1)}
+        avatar={faker.image.avatar()}
+        content={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        author={faker.name.findName()}
+        date={faker.date.past(1)}
+        avatar={faker.image.avatar()}
+        content={faker.lorem.sentence()}
+      />
     </div>
   );
 }
